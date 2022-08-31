00ecc9f3c63ee2bb446a0f431a56331e250acc9a
BuzinszkiCintia <buzinszki.cintia@gmail.com>
2022-08-31 17:20:55 +0200

    modified Home component styling

--- src/components/Home/Home-styles.js
+++ src/components/Home/Home-styles.js
@@ -1,6 +1,65 @@
 import styled from "styled-components";
 
+export const MainContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
+
+export const Navbar = styled.div`
+  width: 100%;
+  position: absolute;
+  top: 0px;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: baseline;
+
+  button {
+    padding-top: 0rem;
+    background-color: transparent;
+    border: none;
+    margin: 0;
+
+    &:hover {
+      background-color: rgba(255, 255, 255, 0.2);
+    }
+  }
+
+  h3 {
+    padding: 1rem;
+    padding-top: 0rem;
+    text-transform: uppercase;
+    font-family: "Roboto Condensed", sans-serif;
+    letter-spacing: 0.1rem;
+  }
+
+  #material-menu {
+    color: white;
+    font-size: 2rem;
+  }
+`;
+
+export const IntroContainer = styled.div`
+  position: absolute;
+  width: 100%;
+  top: 30%;
+
+  h1 {
+    font-family: "Courgette", cursive;
+  }
+  h4 {
+    font-family: "Roboto Condensed", sans-serif;
+    text-transform: uppercase;
+  }
+`;
+
 export const ProfileImg = styled.img`
-  width: 10rem;
-  height: auto;
+  width: 15rem;
+  border-radius: 1.5rem;
+  box-shadow: 0.5rem 0.5rem 0.3rem rgba(43, 75, 71, 0.4);
+`;
+
+export const NavbarImg = styled.img`
+  width: 100%;
 `;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,26 +1,33 @@
 import profileImage from "../../images/profile.jpg";
-import { ProfileImg } from "./Home-styles";
+import navbarImg from "../../images/IMG_1395.PNG";
+import {
+  NavbarImg,
+  ProfileImg,
+  Navbar,
+  IntroContainer,
+  MainContainer,
+} from "./Home-styles";
 
 const Home = () => {
   return (
-    <div className="main-container">
-      <div className="navbar-container">
+    <MainContainer>
+      <Navbar>
         <h3>Cintia Buzinszki | Junior Frontend developer</h3>
-        <div class="material-icons md-36" id="material-menu">
-          menu
-        </div>
-        <div className="img-container">
-          <ProfileImg src={profileImage} alt="profile-img" />
-        </div>
-        <div className="intro-container">
-          <h2>Hi, My Name is Cintia Buzinszki.</h2>
-          <h4>
-            Budapest based | Soon to be Frontend developer | Hobby digital
-            artist |
-          </h4>
-        </div>
-      </div>
-    </div>
+        <button class="material-icons md-36" id="material-menu">
+          <div class="material-icons md-36" id="material-menu">
+            menu
+          </div>
+        </button>
+      </Navbar>
+      <NavbarImg src={navbarImg} alt="navbar-img" />
+      <IntroContainer>
+        <ProfileImg src={profileImage} alt="profile-img" />
+        <h1>Hi, My Name is Cintia Buzinszki.</h1>
+        <h4>
+          | Budapest based | Junior Frontend developer | Hobby digital artist |
+        </h4>
+      </IntroContainer>
+    </MainContainer>
   );
 };
 
