c6064c057cdd0a0110b54f7083624e80cfd2544a
BuzinszkiCintia <buzinszki.cintia@gmail.com>
2022-08-31 17:21:09 +0200

    add MiddleNavbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Home from "./components/Home/Home";
+import MiddleNavbar from "./components/MiddleNavbar/MiddleNavbar";
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <MiddleNavbar />
     </div>
   );
 }
